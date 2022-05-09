f455839e57fc3df39457d5c2da7ea6664abcf162
Devika Sujith <devikasujith12@gmail.com>
2022-05-09 16:56:22 +0400

    Complete project

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const elBtnRating = document.querySelectorAll('circle--rating');
+const elBtnRating = document.querySelectorAll('.circle--rating');
+const elBtnSubmit = document.querySelector('.btn--submit');
+const elContRating = document.querySelector('.selected-rating-container');
+const sectionDefault = document.querySelector('.section-default');
+const sectionTriggered = document.querySelector('.section-triggered');
 
-console.log('hi')
+console.log(elContRating);
 
-for (const btn of elBtnRating) {
-    btn.addEventListener('click', () => {
-        let selected_rating = btn.innerText;
+let selected_rating = 0
 
-        console.log('hi')
-    })
+sectionTriggered.style.display = 'none';
+sectionDefault.style.display = 'block';
+
+for (let btn_rating of elBtnRating) {
+  btn_rating.addEventListener('click', function() {
+    selected_rating = Number(btn_rating.innerHTML);
+    return selected_rating
+  })
 }
+
+
+  elBtnSubmit.addEventListener('click', function() {
+      elContRating.innerHTML = `You selected ${selected_rating} out of 5`
+      
+      sectionDefault.style.display = 'none';
+      sectionTriggered.style.display = 'flex';
+  })
+
+
+
+
